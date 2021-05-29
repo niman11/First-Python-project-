cd2e6d1f260c5f7e9556429709767fd4683e84d3
niman11 <85001597+niman11@users.noreply.github.com>
2021-05-29 16:06:00 -0400

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,17 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello! I'm going to give you questions to get to know you!" )
+Hello! I'm going to give you questions to get to know you!
+>>> name = input("What is your name? ")
+What is your name? Niman
+>>> print("Hello " + name + "! ")
+Hello Niman! 
+>>> age = input("What is your age? ")
+What is your age? 12
+>>> print("You are " + name + " and you are " + age + " years old! ")
+You are Niman and you are 12 years old! 
+>>> hobby = input("What is your hobby? ")
+What is your hobby? Gaming
+>>> print("You are " + name + " and you are " + age + " years old! Your favorite thing to do is, " + hobby + "! ")
+You are Niman and you are 12 years old! Your favorite thing to do is, Gaming! 
+>>> 
